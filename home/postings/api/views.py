4ff5e5ef414f83d19111306b73a2a4e33cf0913e
elmita baidhya <elmita.baidhya@edu.turkuamk.fi>
2021-08-23 19:05:15 +0300

    Initial Commit

---
+++ home/postings/api/views.py
@@ -0,0 +1,14 @@
+# generic
+
+from rest_framework import generics
+
+from postings.models import BlogPost
+from .serializers import BlogPostSerializer
+
+
+class BlogPostRudView(generics.RetrieveUpdateDestroyAPIView):
+    lookup_field = 'pk'
+    serializer_class = BlogPostSerializer
+
+    def get_queryset(self):
+        return BlogPost.objects.all()
